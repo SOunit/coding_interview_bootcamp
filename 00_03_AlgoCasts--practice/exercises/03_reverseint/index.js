1167078926062e3c9c4d97a9abcf6ed051466514
SOunit <workbookwork7@gmail.com>
2021-08-09 02:56:03 -0700

    add practice

--- 00_03_AlgoCasts--practice/exercises/03_reverseint/index.js
+++ 00_03_AlgoCasts--practice/exercises/03_reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const isMinus = n < 0 ? true : false;
+
+  if (isMinus) {
+    n = n * -1;
+  }
+
+  const strNum = '' + n;
+  let reversed = strNum.split('').reverse().join('');
+
+  if (isMinus) {
+    reversed = '-' + reversed;
+  }
+
+  return parseInt(reversed);
+}
 
 module.exports = reverseInt;
