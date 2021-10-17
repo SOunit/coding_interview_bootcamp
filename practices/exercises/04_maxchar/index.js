9cb242906f76a01789306672dd1efb9629a9a6b4
SOunit <workbookwork7@gmail.com>
2021-10-16 17:06:36 -0700

    add asnwer

--- practices/exercises/04_maxchar/index.js
+++ practices/exercises/04_maxchar/index.js
@@ -5,55 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// function maxChar(str) {
-//   const charToCount = new Map();
-
-//   str.split('').forEach((element) => {
-//     if (charToCount.has(element)) {
-//       let count = charToCount.get(element);
-//       count++;
-//       charToCount.set(element, count);
-//     } else {
-//       charToCount.set(element, 1);
-//     }
-//   });
-
-//   console.log(charToCount);
-
-//   let max = 0;
-//   let maxKey = '';
-//   charToCount.forEach((value, key) => {
-//     if (value > max) {
-//       max = value;
-//       maxKey = key;
-//     }
-//   });
-
-//   return maxKey;
-// }
-
 function maxChar(str) {
-  let obj = {};
-  str.split('').forEach((element) => {
-    console.log(element);
-    if (obj[element]) {
-      obj[element]++;
+  const chars = str.split('');
+
+  let charToCount = {};
+  for (let char of chars) {
+    if (charToCount[char]) {
+      charToCount[char]++;
     } else {
-      obj[element] = 1;
+      charToCount[char] = 1;
     }
-  });
+  }
+  console.log(charToCount);
 
-  let max = 0;
-  let maxKey = '';
-  Object.keys(obj).forEach((key) => {
-    const count = obj[key];
-    if (count > max) {
-      max = count;
-      maxKey = key;
+  let maxCount = 0;
+  let maxChar = '';
+  for (let char in charToCount) {
+    if (charToCount[char] > maxCount) {
+      maxCount = charToCount[char];
+      maxChar = char;
     }
-  });
+  }
 
-  return maxKey;
+  console.log(maxChar);
+
+  return maxChar;
 }
 
 module.exports = maxChar;
