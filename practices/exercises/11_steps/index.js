9e552cada59ae4b21164f5612c200185281fc049
SOunit <workbookwork7@gmail.com>
2021-10-29 10:48:22 -0700

    add answer

--- practices/exercises/11_steps/index.js
+++ practices/exercises/11_steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  let str = '';
+  for (let i = 0; i < n; i++) {
+    const row = i + 1;
+    for (let j = 0; j < n; j++) {
+      const column = j + 1;
+      if (column <= row) {
+        str += '#';
+      } else {
+        str += ' ';
+      }
+    }
+    console.log(str);
+    str = '';
+  }
+}
+
+// original 2020/10/28
+// 1. create #
+// 2. add ' ' until n
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let str = '';
+//     for (let j = 0; j < i + 1; j++) {
+//       str += '#';
+//     }
+//     str = format(str, n);
+//     console.log(str);
+//     str = '';
+//   }
+// }
+
+// function format(str, n) {
+//   while (str.length < n) {
+//     str += ' ';
+//   }
+//   return str;
+// }
 
 module.exports = steps;
