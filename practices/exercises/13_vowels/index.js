dc6c6378ac2a2ded3e918ff15d8598170412d490
SOunit <workbookwork7@gmail.com>
2021-12-06 16:30:31 -0800

    change folder name

--- practices/exercises/13_vowels/index.js
+++ practices/exercises/13_vowels/index.js
@@ -7,6 +7,52 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {}
+
+// original loop
+// function vowels(str) {
+//   const obj = {};
+
+//   str.split('').map((char) => {
+//     if (obj[char]) {
+//       obj[char] += 1;
+//     } else {
+//       obj[char] = 1;
+//     }
+//   });
+
+//   console.log(obj);
+
+//   let count = 0;
+//   for (key in obj) {
+//     const lowerCaseKey = key.toLowerCase();
+//     console.log(lowerCaseKey);
+//     if (
+//       lowerCaseKey === 'a' ||
+//       lowerCaseKey === 'e' ||
+//       lowerCaseKey === 'i' ||
+//       lowerCaseKey === 'o' ||
+//       lowerCaseKey === 'u'
+//     ) {
+//       count += obj[key];
+//     }
+//   }
+
+//   return count;
+// }
+
+// original regex
+// function vowels(str) {
+//   console.log(str);
+//   const newStr = str.replace(/[a | i | e | o | u | A | I | E | O | U]/g, '');
+//   return str.length - newStr.length;
+// }
+
+// function vowels(str) {
+//   console.log(str);
+//   const newStr = str.replace(/[^aeiou]/gi, '');
+//   console.log(newStr);
+//   return newStr.length;
+// }
 
 module.exports = vowels;
